96fa3a133d5ed0c023d83f9d230d18303a5946f4
Pedro Neres <pedroneres@Pedros-MacBook-Air.local>
2024-06-05 10:34:12 -0300

    upadted services

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../components/Hero.css"
+import Services from './Services'
 
 function Hero() {
   return (
     <div className='hero'>
         <h1>Building Brands to success</h1>
         <p>A solução que integra marketing, tecnologia e estratégia, que a sua empresa precisa.</p>
+        <Services/>
     </div>
   )
 }
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,56 @@
 import React from 'react'
+import "../components/Services.css"
 
 function Services() {
   return (
     <div className='services'>
-      
-    </div>
+      <div className='services_Content'>
+        <div className='services_Card'>
+          <div className='services_Header'>
+            <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
+            </svg>
+          <h1>Personality</h1>
+          </div>
+          <p>ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+            ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+            </p>
+        </div>
+        <div className='services_Card'>
+          <div className='services_Header'>
+            <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
+            </svg>
+          <h1>Personality</h1>
+          </div>
+          <p>ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+            ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+            </p>
+        </div>
+        <div className='services_Card'>
+          <div className='services_Header'>
+            <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
+            </svg>
+          <h1>Personality</h1>
+          </div>
+          <p>ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+            ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+            </p>
+        </div>
+        <div className='services_Card'>
+          <div className='services_Header'>
+            <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
+            </svg>
+          <h1>Personality</h1>
+          </div>
+          <p>ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+            ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+            </p>
+        </div>
+      </div>
+      </div>
   )
 }
 
