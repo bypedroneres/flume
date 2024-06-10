a3f0d99117286307e77ecef0a8cd20b12739dc3b
Pedro Neres <pedroneres@Pedros-MacBook-Air.local>
2024-06-10 13:51:32 -0300

    updated services

--- src/components/Services.js
+++ src/components/Services.js
@@ -12,8 +12,7 @@ function Services() {
             </svg>
           <h1>Personality</h1>
           </div>
-          <p>ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
-            ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+          <p>Personalizamos totalmente a solução que vamos entregar para a sua empresa, de acordo com as necessidades sob medida para ela.
             </p>
         </div>
         <div className='services_Card'>
@@ -21,10 +20,9 @@ function Services() {
             <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
             </svg>
-          <h1>Personality</h1>
+          <h1>Marketing Digital</h1>
           </div>
-          <p>ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
-            ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+          <p>Oferecemos um pacote completo de marketing digital, incluindo tráfego pago, copywriting, webdesign e design, para maximizar suas vendas e presença online.
             </p>
         </div>
         <div className='services_Card'>
@@ -32,10 +30,9 @@ function Services() {
             <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
             </svg>
-          <h1>Personality</h1>
+          <h1>Tráfego Pago</h1>
           </div>
-          <p>ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
-            ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+          <p>Especialistas em Meta Ads, Google Ads, TikTok Ads, Kwai Ads e LinkedIn Ads, criamos campanhas otimizadas para gerar resultados e aumentar seu alcance.
             </p>
         </div>
         <div className='services_Card'>
@@ -43,10 +40,49 @@ function Services() {
             <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
             </svg>
-          <h1>Personality</h1>
+          <h1>Branding</h1>
           </div>
-          <p>ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
-            ahsakshj ahsakjshjka hahjkskhjashjkahjks hahshasjkhakjskakshja 
+          <p>Criamos e revitalizamos marcas, desenvolvendo logos, identidade visual, realizando rebranding para fortalecer a imagem e posicionamento da sua empresa.
+            </p>
+        </div>
+        <div className='services_Card'>
+          <div className='services_Header'>
+            <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
+            </svg>
+          <h1>Produção de Conteúdo</h1>
+          </div>
+          <p>Gerenciamos suas redes sociais, criamos conteúdo gerado por usuários (UGC) e implementamos estratégias de marketing de influência para engajar seu público.
+            </p>
+        </div>
+        <div className='services_Card'>
+          <div className='services_Header'>
+            <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
+            </svg>
+          <h1>Tech</h1>
+          </div>
+          <p>Fornecemos soluções de programação personalizadas para atender às necessidades específicas do seu negócio, garantindo eficiência e inovação tecnológica.
+            </p>
+        </div>
+        <div className='services_Card'>
+          <div className='services_Header'>
+            <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
+            </svg>
+          <h1>Web Design</h1>
+          </div>
+          <p>Desenvolvemos sites e ecommerces, funcionais e atrativos, focados em proporcionar a melhor experiência ao usuário e converter visitantes em clientes.
+            </p>
+        </div>
+        <div className='services_Card'>
+          <div className='services_Header'>
+            <svg width="25" height="28" viewBox="0 0 25 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.5 28C19.4036 28 25 22.7794 25 16.3396C25 10.5196 21.8209 5.9704 19.6281 3.80234C19.2214 3.4002 18.5442 3.57787 18.3144 4.09559C17.1472 6.72451 14.7153 10.5094 11.6072 10.5094C9.68359 10.7658 6.74513 9.12869 9.11692 1.00906C9.33048 0.27795 8.54644 -0.309349 7.94758 0.179499C4.53844 2.96234 0 8.57334 0 16.3396C0 22.7794 5.59644 28 12.5 28Z" fill="white"/>
+            </svg>
+          <h1>Growth</h1>
+          </div>
+          <p> Elaboramos estratégias de crescimento acelerado, utilizando análise de dados e técnicas avançadas para impulsionar o desenvolvimento e sucesso do seu negócio.
             </p>
         </div>
       </div>
